62155f83997d0e2402a25457e53709a288d28dac
Danielle Branch <dbranch82@gmail.com>
2017-07-10 16:17:34 -0600

    created image file

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>About Danielle</title>
+	</head>
+	<body><p>I am 34 years old. I am raising eight children. I have recently lost 175 pounds and am excited about my new
+	lifestyle and the new opportunities that have presented themselves this year!</p>
+		<img src="images/danielleface.jpg">
+	</body>
+</html>
