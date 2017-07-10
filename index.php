40baf9612823e30a60b82f10bcbdf6f061a73db4
Danielle Branch <dbranch82@gmail.com>
2017-07-10 16:50:12 -0600

    created image file

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 	</head>
 	<body><p>I am 34 years old. I am raising eight children. I have recently lost 175 pounds and am excited about my new
 	lifestyle and the new opportunities that have presented themselves this year!</p>
+		<p>I have a wonderful husband who helps me care for my children while I pursue my dreams. He is from Acoma pueblo
+		some of our favorite foods to cook are fry bread and enchiladas</p>
 		<img src="images/danielleface.jpg">
 	</body>
 </html>
